6a2d73dc8f9e0607299b28c0de244ff7dd02b784
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-22 02:28:10 +0430

    Modify the Players and Player components to implement the drag and drop ordering functionality to the players list.

--- src/pages/SelectPlayers/Player.tsx
+++ src/pages/SelectPlayers/Player.tsx
@@ -1,35 +1,86 @@
 import React from "react";
 import styled from "styled-components";
 import {colors} from "../../utilities";
+import {Draggable} from "react-beautiful-dnd";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowsAltV} from "@fortawesome/free-solid-svg-icons";
 
+// TODO: Should get an intersection of Player type and its props.
 type Props = {
     selected: boolean,
-    name: string
+    name: string,
+    id: string,
+    index: number,
 }
-const Player = ({selected, name}: Props) => {
+const Player = ({selected, name, id, index}: Props) => {
     return (
-        <PlayerComponent selected={selected}>
-            <PlayerName>{name}</PlayerName>
-        </PlayerComponent>
+        <Draggable draggableId={id} index={index} isDragDisabled={!selected}>
+            {(provided, snapshot) =>
+                <PlayerComponent {...provided.draggableProps}
+                                 ref={provided.innerRef}
+                                 selected={selected}
+                                 dragging={snapshot.isDragging}>
+                    <PlayerName>{name}</PlayerName>
+                    <SelectButton/>
+                    <DragHandle {...provided.dragHandleProps}>
+                        <FontAwesomeIcon icon={faArrowsAltV}/>
+                    </DragHandle>
+                </PlayerComponent>
+            }
+        </Draggable>
     );
 };
 
 type PlayerItemProps = {
-    selected: boolean
+    selected: boolean,
+    dragging: boolean,
 }
-const PlayerComponent = styled.button<PlayerItemProps>`
-  display: flex;
+const PlayerComponent = styled.div<PlayerItemProps>`
+  display: grid;
+  grid-template-columns: 6fr 1fr;
   justify-content: center;
   align-items: center;
   margin: .5rem;
-  padding: .5rem;
-  background-color: ${props => props.selected ? colors.secondary : colors.primary};
+  background-color: ${
+          props => props.selected ?
+                  props.dragging ? colors.secondaryDark : colors.secondary
+                  : colors.primary};
   opacity: ${props => props.selected ? 1 : .6};
 `;
 
-const PlayerName = styled.p`
+const PlayerName = styled.h1`
+  grid-column: 1/3;
+  grid-row: 1;
+  justify-self: center;
   font-size: 1.2rem;
   color: ${colors.white};
+`;
+
+const SelectButton = styled.button`
+  grid-column: 1/2;
+  grid-row: 1;
+  justify-self: stretch;
+  align-self: stretch;
+  background-color: transparent;
+`;
+
+const DragHandle = styled.button`
+  grid-column: 2/3;
+  grid-row: 1;
+  justify-self: center;
+  align-self: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: .2rem;
+  width: 2rem;
+  height: 2rem;
+  background-color: ${colors.primary};
+
+  svg {
+    font-size: 1.2rem;
+    color: ${colors.white};
+  }
 `;
 
 export default Player;
--- src/pages/SelectPlayers/Players.tsx
+++ src/pages/SelectPlayers/Players.tsx
@@ -1,34 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import Player from "./Player";
 import {v4 as uuid} from "uuid";
+import {DragDropContext, Droppable, DropResult} from "react-beautiful-dnd";
 
 const Players = () => {
     // TODO: Implement the actual players instead of using names
-    const names = [
-        {name: "سید محمد", id: uuid()},
-        {name: "سید علی", id: uuid()},
-        {name: "سید مجتبی", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "سید علی میری", id: uuid()},
-        {name: "دایی", id: uuid()},
-        {name: "آقا محسن", id: uuid()},
-        {name: "آقا محمد", id: uuid()},
-        {name: "سید حسین", id: uuid()},
+    const selectedGenerator = () => Math.random() > .5;
+    const playersInitial = [
+        {name: "سید محمد", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی", id: uuid(), selected: selectedGenerator()},
+        {name: "سید مجتبی", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "سید علی میری", id: uuid(), selected: selectedGenerator()},
+        {name: "دایی", id: uuid(), selected: selectedGenerator()},
+        {name: "آقا محسن", id: uuid(), selected: selectedGenerator()},
+        {name: "آقا محمد", id: uuid(), selected: selectedGenerator()},
+        {name: "سید حسین", id: uuid(), selected: selectedGenerator()},
     ];
+    const [players, setPlayers] = useState(playersInitial);
+
+    const onDragEnd = ({draggableId, destination, source}: DropResult) => {
+        if (!destination) return;
+        if (source.droppableId === destination.droppableId && source.index === destination.index) return;
+
+        const item = players.find(player => player.id === draggableId);
+        if (!item) return;
+
+        const newPlayers = Array.from(players);
+        newPlayers.splice(source.index, 1);
+        newPlayers.splice(destination.index, 0, item);
+        setPlayers(newPlayers);
+    };
+
     return (
-        <PlayersComponent>
-            {names.map((player, index) =>
-                <Player key={index} selected={Math.random() > 0.5} name={player.name}/>)}
-        </PlayersComponent>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId={uuid()}>
+                {(provided) =>
+                    <PlayersComponent {...provided.droppableProps} ref={provided.innerRef}>
+                        {players.map((player, index) =>
+                            <Player key={player.id} {...player} index={index}/>)}
+                        {provided.placeholder}
+                    </PlayersComponent>
+                }
+            </Droppable>
+        </DragDropContext>
     );
 };
 
