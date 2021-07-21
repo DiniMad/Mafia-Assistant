c542124176b4afd61c89a83eff804c0e5dcb69f2
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-21 15:46:45 +0430

    Add the color palette file.

---
+++ src/utilities/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+    primary: "#272727",
+    primaryDark:"#000000",
+    primaryLight:"484848",
+    secondary:"#ef6c00",
+    secondaryDark:"#b53d00",
+    secondaryLight:"#ff9d3f",
+    white: "#e5e5e5"
+};
+
+export default colors;
