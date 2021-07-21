064a18029db8f98e284b3e4f341cef4e1c2ce723
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-21 17:27:38 +0430

    Fix a bug in the GlobalStyle component which cause wrong font size.

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,11 +1,14 @@
 import {createGlobalStyle} from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    font-size: 10px;
+  * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+  
+  body{
+    font-size: 10px;
   }
 
   button {
