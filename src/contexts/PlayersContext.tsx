798bb21b5ad277eea6707bb6d33b3b999c884768
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-22 17:08:33 +0430

    Modify the PlayersContext to sort players before initialization.

--- src/contexts/PlayersContext.tsx
+++ src/contexts/PlayersContext.tsx
@@ -29,7 +29,10 @@ const reducer = (state: Player[], action: PlayerAction) => {
 type PlayerContextType = [Player[], Dispatch<PlayerAction>];
 export const PlayersContext = createContext<PlayerContextType>({} as PlayerContextType);
 export const PlayersProvider: FC = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, mockPlayers);
+    const initialState = mockPlayers.sort((p1, p2) => {
+        return (p1.selected === p2.selected) ? 0 : p1.selected ? -1 : 1;
+    });
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     return (
         <PlayersContext.Provider value={[state, dispatch]}>
