11b05a2098ee559a924e2591ced8bc652c25f004
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-23 11:47:07 +0430

    Create a component for players list in TalkRoom.

---
+++ src/pages/TalkRoom/Players.tsx
@@ -0,0 +1,29 @@
+import React, {useContext} from "react";
+import styled from "styled-components";
+import GameplayContext from "../../contexts/GameplayContext";
+import Player from "./Player";
+
+const Players = () => {
+    const [{players, displayRoles}] = useContext(GameplayContext);
+    
+    return (
+        <PlayersComponent>
+            {players.map(player =>
+                <Player key={player.id}
+                        player={player}
+                        displayRole={displayRoles}
+                        timeToTalk={player.talking ? "30" : undefined}/>)}
+        </PlayersComponent>
+    );
+};
+
+export const PlayersComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  overflow-y: scroll;
+  overflow-x: hidden;
+`;
+
+export default Players;
