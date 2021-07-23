e11a164bbaed5965131064d8c5a7103a7b81b5a3
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-23 11:41:30 +0430

    Add the Gameplay provider to the Providers component.

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,10 +1,13 @@
 import React, {FC} from "react";
 import {PlayersProvider} from "./contexts/PlayersContext";
+import {GameplayProvider} from "./contexts/GameplayContext";
 
 const Providers: FC = ({children}) => {
     return (
         <PlayersProvider>
-            {children}
+            <GameplayProvider>
+                {children}
+            </GameplayProvider>
         </PlayersProvider>
     );
 };
