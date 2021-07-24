2e26af15b8a24159b973cc5848ec04a7911fded0
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-25 01:55:59 +0430

    Add routes to the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import GlobalStyle from "./GlobalStyle";
-import {colors, assets} from "./utilities";
-import SelectPlayers from "./pages/SelectPlayers/SelectPlayers";
+import {colors, assets, routes} from "./utilities";
 import Providers from "./Providers";
+import StartPage from "./pages/Start";
+import SelectPlayers from "./pages/SelectPlayers";
+import SelectRoles from "./pages/SelectRoles";
+import RevealRoles from "./pages/RevealRoles";
+import TalkRoom from "./pages/TalkRoom";
+import ModifyPlayers from "./pages/ModifyPlayers";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 const App = () => {
     return (
-        <AppComponent>
-            <Providers>
-                <SelectPlayers/>
-                <GlobalStyle/>
-            </Providers>
-        </AppComponent>
+        <Router>
+            <AppComponent>
+                <Providers>
+                    <Switch>
+                        <Route exact path={routes.start}> <StartPage/> </Route>
+                        <Route exact path={routes.selectPlayers}> <SelectPlayers/> </Route>
+                        <Route exact path={routes.selectRoles}> <SelectRoles/> </Route>
+                        <Route exact path={routes.revealRoles}> <RevealRoles/> </Route>
+                        <Route exact path={routes.talkRoom}> <TalkRoom/> </Route>
+                        <Route exact path={routes.modifyPlayers}> <ModifyPlayers/> </Route>
+                    </Switch>
+                    <GlobalStyle/>
+                </Providers>
+            </AppComponent>
+        </Router>
     );
 };
 const AppComponent = styled.div`
