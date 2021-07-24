0184fb9d2243a5da9047ace5357b0291424688f6
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-24 22:46:18 +0430

    Create the RevealRoles page component itself.

---
+++ src/pages/RevealRoles/RevealRoles.tsx
@@ -0,0 +1,87 @@
+import React, {useContext, useEffect, useState} from "react";
+import PageLayout from "../../components/PageLayout";
+import styled, {keyframes} from "styled-components";
+import {colors} from "../../utilities";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowLeft, faUndo, faComments} from "@fortawesome/free-solid-svg-icons";
+import useInitializePlayers from "./hooks/useInitializePlayers";
+import {GameplayContext} from "../../contexts/GameplayContext";
+import Player from "./Player";
+
+
+const RevealRoles = () => {
+    const [{players}, dispatch] = useContext(GameplayContext);
+    const [initialPlayers, shuffleRoles] = useInitializePlayers();
+    const [reshuffleClasses, setReshuffleClasses] = useState<"animation" | undefined>(undefined);
+    const allRolesRevealed = players.findIndex(player => !player.role.revealed) === -1;
+
+    useEffect(() => {
+        dispatch({
+            type: "SET_PLAYERS",
+            payload: initialPlayers,
+        });
+    }, [initialPlayers]);
+
+    const onReshuffleButtonClicked = () => {
+        shuffleRoles();
+        setReshuffleClasses("animation");
+    };
+    const onReshuffleAnimationEnded = () => setReshuffleClasses(undefined);
+
+    return (
+        <PageLayout pageTitle={"اطلاق نقش"}>
+            {() => {
+                return {
+                    content:
+                        <PlayersComponent>
+                            {players.map(player => <Player key={player.id} {...player}/>)}
+                        </PlayersComponent>,
+                    menuContent: <>
+                        <MenuButton>
+                            <FontAwesomeIcon icon={faArrowLeft}/>
+                        </MenuButton>
+                        <MenuButton onClick={onReshuffleButtonClicked}>
+                            <FontAwesomeIcon icon={faUndo}
+                                             className={reshuffleClasses}
+                                             onAnimationEnd={onReshuffleAnimationEnded}/>
+                        </MenuButton>
+                        <MenuButton highlightColor={allRolesRevealed}>
+                            <FontAwesomeIcon icon={faComments}/>
+                        </MenuButton>
+                    </>,
+                };
+            }}
+        </PageLayout>
+    );
+};
+
+const PlayersComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  overflow-y: scroll;
+  overflow-x: hidden;
+`;
+
+type MenuButtonProps = {
+    highlightColor?: boolean
+}
+const MenuButton = styled.button<MenuButtonProps>`
+  font-size: 1.9rem;
+  font-weight: bold;
+  color: ${colors.white};
+  width: 4rem;
+  height: 4rem;
+  background-color: ${props => props.highlightColor ? colors.secondary : "transparent"};
+
+  svg.animation {
+    animation: ${keyframes`
+      100% {
+        transform: rotate(-360deg);
+      }
+    `} .3s forwards;;
+  }
+`;
+
+export default RevealRoles;
