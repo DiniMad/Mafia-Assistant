12f193a58f7a2b7b1145f67bc1b49b57b87521d6
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-24 23:16:01 +0430

    Add a constraint into the SelectPlayers page to prevent selecting less players than necessary.

--- src/pages/SelectPlayers/SelectPlayers.tsx
+++ src/pages/SelectPlayers/SelectPlayers.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useContext} from "react";
 import styled from "styled-components";
 import {colors} from "../../utilities";
 import Players from "./Players";
 import PageLayout from "../../components/PageLayout";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faUserSecret,
+    faUserMd,
+    faUserGraduate,
+    faUser,
+    faArrowLeft,
+    faUserEdit,
+} from "@fortawesome/free-solid-svg-icons";
+import {PersistentPlayersContext} from "../../contexts/PersistentPlayersContext";
 
 const SelectPlayers = () => {
+    const [players] = useContext(PersistentPlayersContext);
+    const MINIMUM_PLAYERS_COUNT = 6 as const;
+    const playersCountAllowed = players.filter(player => player.selected).length >= MINIMUM_PLAYERS_COUNT;
+
+    const onSelectRolesPageButtonClicked = () => console.log("SelectRoles");
+
     return (
         <PageLayout pageTitle={"انتخاب بازیکنان"}>
             {
@@ -12,9 +28,20 @@ const SelectPlayers = () => {
                     return {
                         content: <Players/>,
                         menuContent: <>
-                            <MenuButton>{"بازگشت"}</MenuButton>
-                            <MenuButton>{"ویرایش بازیکنان"}</MenuButton>
-                            <MenuButton>{"ادامه"}</MenuButton>
+                            <MenuButton>
+                                <FontAwesomeIcon icon={faArrowLeft}/>
+                            </MenuButton>
+                            <MenuButton>
+                                <FontAwesomeIcon icon={faUserEdit}/>
+                            </MenuButton>
+                            <MenuButton highlight={playersCountAllowed}
+                                        disabled={!playersCountAllowed}
+                                        onClick={onSelectRolesPageButtonClicked}>
+                                <FontAwesomeIcon className={"roles-button"} icon={faUserSecret}/>
+                                <FontAwesomeIcon className={"roles-button"} icon={faUserMd}/>
+                                <FontAwesomeIcon className={"roles-button"} icon={faUserGraduate}/>
+                                <FontAwesomeIcon className={"roles-button"} icon={faUser}/>
+                            </MenuButton>
                         </>,
                     };
                 }
@@ -23,11 +50,42 @@ const SelectPlayers = () => {
     );
 };
 
-const MenuButton = styled.button`
+type MenuButtonProps = {
+    highlight?: boolean
+}
+const MenuButton = styled.button<MenuButtonProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-size: 1.9rem;
   font-weight: bold;
   color: ${colors.white};
   padding: .5rem 1.2rem;
+  width: 4rem;
+  height: 4rem;
+  background-color: ${props => props.highlight ? colors.secondary : "transparent"};
+  transition: background-color .5s;
+
+  .roles-button {
+    position: absolute;
+    font-size: 1.5rem;
+
+    &:nth-of-type(1) {
+      transform: translate(-.8rem, -.8rem);
+    }
+
+    &:nth-of-type(2) {
+      transform: translate(.8rem, -.8rem);
+    }
+
+    &:nth-of-type(3) {
+      transform: translate(-.8rem, .8rem);
+    }
+
+    &:nth-of-type(4) {
+      transform: translate(.8rem, .8rem);
+    }
+  }
 `;
 
 export default SelectPlayers;
