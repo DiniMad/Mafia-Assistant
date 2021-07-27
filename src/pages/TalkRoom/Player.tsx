071989a6fd39e2f11f7e6d8eca2765d231c88dd7
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-27 12:57:47 +0430

    Modify the Player component to use the hooks.

--- src/pages/TalkRoom/Player.tsx
+++ src/pages/TalkRoom/Player.tsx
@@ -1,41 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
-import {colors} from "../../utilities";
+import {colors, colorWithOpacity} from "../../utilities";
 import {GameplayPlayer} from "../../types/Gameplay";
-import {colorWithOpacity} from "../../utilities/colors";
+import PlayerOptions from "./PlayerOptions";
+import useComponentState from "./hooks/useComponentState";
+import useLongPress from "./hooks/useLongPress";
+import useTalk from "./hooks/useTalk";
 
 type Props = {
     player: GameplayPlayer,
-    timeToTalk?: string,
     displayRole: boolean,
 }
-const Player = ({player, timeToTalk, displayRole}: Props) => {
+const Player = ({player, displayRole}: Props) => {
+    const [displayOptions, setDisplayOptions] = useState(false);
+    const {onClick, onSideActionClicked, talkTime} = useTalk(player);
+    const longPressProperties = useLongPress({delay: 300, onClick, onLongPress});
+    const {componentClasses, sideActionStatus} = useComponentState({player, talkTime});
+
+    function onLongPress() {
+        setDisplayOptions(true);
+    }
+
     return (
-        <PlayerComponent talking={player.talking} talked={player.talked}>
+        <PlayerComponent className={componentClasses}>
             <PlayerRole displayRole={displayRole}>{player.role.shortName}</PlayerRole>
-            <PlayerName>{player.name}</PlayerName>
-            <PlayerSideAction talking={player.talking}>
-                {player.talking ? timeToTalk : "چالش"}
-            </PlayerSideAction>
+            <PlayerName disabled={!player.active} {...longPressProperties}>{player.name}</PlayerName>
+            <SideAction disabled={sideActionStatus.disabled} onClick={onSideActionClicked}>
+                {sideActionStatus.text}
+            </SideAction>
+            <PlayerOptions display={displayOptions} setDisplay={setDisplayOptions} player={player}/>
         </PlayerComponent>
     );
 };
-type PlayerItemProps = Pick<GameplayPlayer, "talked" | "talking">;
-const PlayerComponent = styled.div<PlayerItemProps>`
+
+const PlayerComponent = styled.div`
   display: grid;
   grid-template-columns: 1fr 5fr 1fr;
   justify-content: center;
   align-items: center;
   margin: .6rem;
   height: 4rem;
-  background-color: ${props =>
-          props.talking ?
-                  colors.secondary :
-                  props.talked ? colorWithOpacity(colors.primary, .6) : colors.primary};
+  background-color: ${colors.primary};
+  transition: background-color .5s;
+
+  &.long-press {
+    background-color: ${colorWithOpacity(colors.primaryDark, .8)};
+  }
+
+  &.de-active, &.talked {
+    background-color: ${colorWithOpacity(colors.primary, .6)};
+
+    &.de-active {
+      border: ${colors.primaryLight} solid .2rem;
+    }
+  }
+
+  &.talking {
+    background-color: ${colors.secondary};
+  }
 `;
 
-type PlayerSideActionProps = Pick<GameplayPlayer, "talking">;
-const PlayerSideAction = styled.button<PlayerSideActionProps>`
+const SideAction = styled.button`
   grid-column: 3/4;
   grid-row: 1;
   justify-self: center;
@@ -44,7 +69,11 @@ const PlayerSideAction = styled.button<PlayerSideActionProps>`
   color: ${colors.white};
   height: 3.5rem;
   width: 3.5rem;
-  background-color: ${props => props.talking ? colors.secondaryDark : colors.primaryLight};
+  background-color: ${colors.primaryLight};
+
+  ${PlayerComponent}.de-active &, ${PlayerComponent}.talking & {
+    background-color: transparent;
+  }
 `;
 
 const PlayerName = styled.button`
@@ -54,6 +83,7 @@ const PlayerName = styled.button`
   align-self: stretch;
   font-size: 1.9rem;
   color: ${colors.white};
+  user-select: none;
 `;
 
 type PlayerRoleProps = Pick<Props, "displayRole">;
@@ -70,4 +100,5 @@ const PlayerRole = styled.h3<PlayerRoleProps>`
   opacity: ${props => props.displayRole ? 1 : 0};
   transition: opacity .3s;
 `;
+
 export default Player;
