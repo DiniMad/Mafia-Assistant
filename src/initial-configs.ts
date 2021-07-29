26081b98fa9c5144ab2a41bf360c355e7ec51231
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-07-29 05:18:25 +0430

    Add some roles.

--- src/initial-configs.ts
+++ src/initial-configs.ts
@@ -4,11 +4,17 @@ import {Config} from "./types/Gameplay";
 export const PLAYER_ROLES: PersistentPlayerRole[] = [
     {name: "رئیس مافیا", shortName: "رئیس", side: "Mafia", variety: "One", selected: false, count: 1},
     {name: "مذاکره کننده", shortName: "مذاکره", side: "Mafia", variety: "One", selected: false, count: 1},
+    {name: "ناتو", shortName: "ناتو", side: "Mafia", variety: "One", selected: false, count: 1},
+    {name: "گروگانگیر", shortName: "گروگانگیر", side: "Mafia", variety: "One", selected: false, count: 1},
+    {name: "دکتر لکتر", shortName: "لکتر", side: "Mafia", variety: "One", selected: false, count: 1},
     {name: "مافیا ساده", shortName: "مافیا", side: "Mafia", variety: "Many", selected: false, count: 1},
     {name: "کاراگاه", shortName: "کاراگاه", side: "Citizen", variety: "One", selected: false, count: 1},
     {name: "پزشک", shortName: "پزشک", side: "Citizen", variety: "One", selected: false, count: 1},
     {name: "تیرانداز", shortName: "تیرانداز", side: "Citizen", variety: "One", selected: false, count: 1},
     {name: "زره پوش", shortName: "زره", side: "Citizen", variety: "One", selected: false, count: 1},
+    {name: "تفنگدار", shortName: "تفنگدار", side: "Citizen", variety: "One", selected: false, count: 1},
+    {name: "نگهبان", shortName: "نگهبان", side: "Citizen", variety: "One", selected: false, count: 1},
+    {name: "خبرنگار", shortName: "خبرنگار", side: "Citizen", variety: "One", selected: false, count: 1},
     {name: "شهروند ساده", shortName: "شهروند", side: "Citizen", variety: "Many", selected: false, count: 1},
 ];
 
