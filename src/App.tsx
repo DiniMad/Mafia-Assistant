25bfa48f4e5047d76866a084f14a2a65a1ea4b27
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-04-06 21:34:05 +0330

    feat: Implementing bear-bone App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import tw from "twin.macro";
+import BackgroundImage from "@images/Background.png";
+import styled from "styled-components";
+
 function App() {
 
     return (
-        <div>
-        </div>
+        <AppComponent>
+
+        </AppComponent>
     );
 }
+
+const AppComponent = styled.div`
+  background: url(${BackgroundImage});
+  ${tw`overflow-hidden w-screen h-screen bg-neutral-800 bg-no-repeat bg-fixed bg-center`}
+`;
 
 export default App;
