c9ae42a35ede29b47b9ee15c3d47585c6213e084
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-04-06 17:01:40 +0330

    feat: Implementing GlobalStyles component.

---
+++ src/components/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+﻿import {GlobalStyles as BaseStyles} from "twin.macro";
+
+const GlobalStyles = () => {
+    return (
+        <>
+            <BaseStyles/>
+        </>
+    );
+};
+
+export default GlobalStyles;
