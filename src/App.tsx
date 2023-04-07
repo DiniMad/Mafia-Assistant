f44f2eb802b655455abacd7c8703e5ac0a4674cd
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-04-07 12:39:09 +0330

    feat: Setting-up react router and home route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import tw from "twin.macro";
 import BackgroundImage from "@images/Background.png";
 import styled from "styled-components";
+import {Route, Routes} from "react-router-dom";
+import Home from "@/pages/home";
 
 function App() {
 
     return (
         <AppComponent>
-
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
         </AppComponent>
     );
 }
 
 const AppComponent = styled.div`
   background: url(${BackgroundImage});
-  ${tw`overflow-hidden w-screen h-screen bg-neutral-800 bg-no-repeat bg-fixed bg-center`}
+  ${tw`overflow-hidden w-screen h-screen bg-neutral-800 bg-no-repeat bg-cover bg-center`}
 `;
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import GlobalStyles from "@components/GlobalStyles";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <GlobalStyles/>
-        <App/>
+        <BrowserRouter> <App/> </BrowserRouter>
     </React.StrictMode>,
 );
