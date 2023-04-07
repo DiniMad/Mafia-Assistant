c5b9d2053c4370ca4ffba3b11fdc8a6cbf7ee551
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-04-07 14:16:17 +0330

    feat: Implementing localization.

---
+++ src/i18n/config.ts
@@ -0,0 +1,22 @@
+﻿import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import faTranslations from "@/i18n/locales/fa/translations.json";
+import enTranslations from "@/i18n/locales/en/translations.json";
+
+export const defaultNS = "translations";
+export const resources = {
+    fa: {translations: {...faTranslations}},
+    en: {translations: {...enTranslations}},
+} as const;
+
+i18n.use(initReactI18next).init({
+    fallbackLng: "fa",
+    lng: "fa",
+    resources,
+    defaultNS,
+    ns: ["translations"],
+});
+
+i18n.languages = ["fa", "en"];
+
+export default i18n;
---
+++ types/i18next.d.ts
@@ -0,0 +1,9 @@
+﻿import "i18next";
+import {resources, defaultNS} from "@/i18n/config";
+
+declare module "i18next" {
+    interface CustomTypeOptions {
+        defaultNS: typeof defaultNS;
+        resources: typeof resources["fa"];
+    }
+}
