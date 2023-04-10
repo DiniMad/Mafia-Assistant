f0d4b64bcb39db6291ea98772254c96be3448127
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-04-10 19:46:30 +0330

    improve: Adding some colors to tailwind.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 ﻿module.exports = {
     theme: {
         extend: {
-            colors: {},
+            colors: {
+                accent: {
+                    200: "#ff9d3f",
+                    300: "#ef6c00",
+                    400: "#b53d00",
+                },
+                background: {
+                    200: "#333333",
+                    300: "#272727",
+                    400: "#000000",
+                }
+            },
         },
     },
     plugins: [],
-}
+};
