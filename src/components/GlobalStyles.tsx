a992642b3eee1b413bf064cebdec0c9b77e48df4
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-04-16 21:05:28 +0330

    style: Adding some global styles to remove number input arrows.

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,11 +1,21 @@
 ﻿import {GlobalStyles as BaseStyles} from "twin.macro";
+import {createGlobalStyle} from "styled-components";
 
 const GlobalStyles = () => {
     return (
         <>
             <BaseStyles/>
+            <CustomStyles/>
         </>
     );
 };
+
+const CustomStyles = createGlobalStyle`
+  input[type="number"]::-webkit-inner-spin-button,
+  input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
 
 export default GlobalStyles;
