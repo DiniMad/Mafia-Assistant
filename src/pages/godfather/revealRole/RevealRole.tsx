1d0a426086999c70263cb3b472938d25a68ae6fe
Seyed Hossein Hosseini <40262172+DiniMad@users.noreply.github.com>
2023-04-25 10:41:41 +0000

    chore: Moving the layout component to the root componnets directory instead of the godfother dircetroy.

--- src/pages/godfather/revealRole/RevealRole.tsx
+++ src/pages/godfather/revealRole/RevealRole.tsx
@@ -1,43 +1,43 @@
-﻿import React, {AnimationEventHandler, MouseEventHandler, useEffect, useState} from "react";
-import tw, {styled} from "twin.macro";
-import {Link} from "react-router-dom";
+﻿import React, { AnimationEventHandler, MouseEventHandler, useEffect, useState } from "react";
+import tw, { styled } from "twin.macro";
+import { Link } from "react-router-dom";
 import appRoutes from "@/utilites/appRoutes";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowLeft, faArrowRight, faUndo} from "@fortawesome/free-solid-svg-icons";
-import Layout from "@/pages/godfather/Layout";
-import {useTranslation} from "react-i18next";
-import {usePlayers} from "@/store/players";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight, faUndo } from "@fortawesome/free-solid-svg-icons";
+import Layout from "@/components/Layout";
+import { useTranslation } from "react-i18next";
+import { usePlayers } from "@/store/players";
 import Player from "@/pages/godfather/revealRole/Player";
-import {useRoles} from "@/store/roles";
-import {useDispatch} from "react-redux";
-import {assignRolesToPlayersRandomly, useGodfatherGamePlayers} from "@/store/godfatherGame";
+import { useRoles } from "@/store/roles";
+import { useDispatch } from "react-redux";
+import { assignRolesToPlayersRandomly, useGodfatherGamePlayers } from "@/store/godfatherGame";
 
 const RoleSelection = () => {
     const [reshuffleAnimation, setReshuffleAnimation] = useState(false);
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const dispatch = useDispatch();
     const players = usePlayers();
     const roles = useRoles();
     const godfatherPlayers = useGodfatherGamePlayers();
 
     useEffect(() => {
-        dispatch(assignRolesToPlayersRandomly({players, roles}));
+        dispatch(assignRolesToPlayersRandomly({ players, roles }));
     }, []);
 
     const shuffleRoles = () => {
-        dispatch(assignRolesToPlayersRandomly({players, roles}));
+        dispatch(assignRolesToPlayersRandomly({ players, roles }));
         setReshuffleAnimation(true);
     };
     const resetReshuffleAnimation = () => setReshuffleAnimation(false);
 
     return (
         <Layout pageTitle={t("assignRole")}
-                bottomMenu={<BottomMenu shuffleRoles={shuffleRoles}
-                                        onShuffleAnimationEnded={resetReshuffleAnimation}
-                                        reshuffleAnimation={reshuffleAnimation}/>}>
+            bottomMenu={<BottomMenu shuffleRoles={shuffleRoles}
+                onShuffleAnimationEnded={resetReshuffleAnimation}
+                reshuffleAnimation={reshuffleAnimation} />}>
             <PlayerList>
-                {godfatherPlayers.map(p => <Player key={p.id} player={p}/>)}
+                {godfatherPlayers.map(p => <Player key={p.id} player={p} />)}
             </PlayerList>
         </Layout>
     );
@@ -46,30 +46,30 @@ const RoleSelection = () => {
 const PlayerList = tw.div`flex flex-col justify-start`;
 
 
-const BottomMenu = ({shuffleRoles, onShuffleAnimationEnded, reshuffleAnimation}: {
+const BottomMenu = ({ shuffleRoles, onShuffleAnimationEnded, reshuffleAnimation }: {
     shuffleRoles: MouseEventHandler<HTMLButtonElement>,
     onShuffleAnimationEnded: AnimationEventHandler<SVGSVGElement>,
     reshuffleAnimation: boolean
 }) =>
     <div tw="flex justify-between items-center h-full w-full">
         <Link tw="flex justify-center items-center h-10 w-10"
-              to={appRoutes.godfather.pathTo(appRoutes.godfather.roleSelection)}>
-            <FontAwesomeIcon icon={faArrowLeft} tw="text-white text-2xl"/>
+            to={appRoutes.godfather.pathTo(appRoutes.godfather.roleSelection)}>
+            <FontAwesomeIcon icon={faArrowLeft} tw="text-white text-2xl" />
         </Link>
         <button tw="flex justify-center items-center h-10 w-10" onClick={shuffleRoles}>
             <ReshuffleIcon icon={faUndo}
-                           animation={reshuffleAnimation}
-                           onAnimationEnd={onShuffleAnimationEnded}/>
+                animation={reshuffleAnimation}
+                onAnimationEnd={onShuffleAnimationEnded} />
         </button>
         <Link tw="flex justify-center items-center h-10 w-10"
-              to={appRoutes.godfather.pathTo(appRoutes.godfather.gameFlow)}>
-            <FontAwesomeIcon icon={faArrowRight} tw="text-white text-2xl"/>
+            to={appRoutes.godfather.pathTo(appRoutes.godfather.gameFlow)}>
+            <FontAwesomeIcon icon={faArrowRight} tw="text-white text-2xl" />
         </Link>
     </div>;
 
 const ReshuffleIcon = styled(FontAwesomeIcon)`
   ${tw`text-white text-2xl`}
-  ${({animation}: { animation: boolean }) => animation && tw`animate-rotate`}
+  ${({ animation }: { animation: boolean }) => animation && tw`animate-rotate`}
 `;
 
 export default RoleSelection;
--- src/pages/godfather/roleSelection/RoleSelection.tsx
+++ src/pages/godfather/roleSelection/RoleSelection.tsx
@@ -1,21 +1,21 @@
 ﻿import React from "react";
 import tw from "twin.macro";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import appRoutes from "@/utilites/appRoutes";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons";
-import Layout from "@/pages/godfather/Layout";
-import {useTranslation} from "react-i18next";
-import {useRoles} from "@/store/roles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Layout from "@/components/Layout";
+import { useTranslation } from "react-i18next";
+import { useRoles } from "@/store/roles";
 import Role from "@/pages/godfather/roleSelection/Role";
 
 const RoleSelection = () => {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const roles = useRoles();
 
     return (
-        <Layout pageTitle={t("roleSelection")} bottomMenu={<BottomMenu/>}>
-            {roles.map(r => <Role key={r.key} role={r}/>)}
+        <Layout pageTitle={t("roleSelection")} bottomMenu={<BottomMenu />}>
+            {roles.map(r => <Role key={r.key} role={r} />)}
         </Layout>
     );
 };
@@ -23,12 +23,12 @@ const RoleSelection = () => {
 const BottomMenu = () =>
     <div tw="flex justify-between items-center h-full w-full">
         <Link tw="flex justify-center items-center h-10 w-10"
-              to={appRoutes.godfather.pathTo("")}>
-            <FontAwesomeIcon icon={faArrowLeft} tw="text-white text-2xl"/>
+            to={appRoutes.godfather.pathTo("")}>
+            <FontAwesomeIcon icon={faArrowLeft} tw="text-white text-2xl" />
         </Link>
         <Link tw="flex justify-center items-center h-10 w-10"
-              to={appRoutes.godfather.pathTo(appRoutes.godfather.revealRoles)}>
-            <FontAwesomeIcon icon={faArrowRight} tw="text-white text-2xl"/>
+            to={appRoutes.godfather.pathTo(appRoutes.godfather.revealRoles)}>
+            <FontAwesomeIcon icon={faArrowRight} tw="text-white text-2xl" />
         </Link>
     </div>;
 
