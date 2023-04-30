1aca76f570c90f668538ba2f0dcd16459633600d
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-04-30 20:05:19 +0330

    feat: Creating the tick state machine definition.

---
+++ src/stateMachines/common/tickStateDefinition.ts
@@ -0,0 +1,35 @@
+﻿import {assign} from "xstate";
+
+export type TickContext = { elapsedTime: number }
+export type TickEvents = { type: "PAUSE" | "RESUME" }
+
+export const resetTick = assign({
+    elapsedTime: () => 0,
+});
+
+const incrementTick = assign({
+    elapsedTime: (context: TickContext) => context.elapsedTime + 1,
+});
+
+export const tickStateDefinition = {
+    id: "tick",
+    initial: "tick",
+    states: {
+        tick: {
+            after: {
+                1000: {
+                    actions: incrementTick,
+                    target: "tick",
+                },
+            },
+            on: {
+                PAUSE: "paused",
+            },
+        },
+        paused: {
+            on: {
+                RESUME: "tick",
+            },
+        },
+    },
+};
